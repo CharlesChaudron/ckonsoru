f97c708ff9a8d9ecbd96a1d16011c27e58b4e3d3
solo-carry <maelbizot23@gmail.com>
2021-05-03 01:49:48 +0200

    azertyu

--- src/main/java/com/fges/ckonsoru/PostgresInteraction.java
+++ src/main/java/com/fges/ckonsoru/PostgresInteraction.java
@@ -1,6 +1,13 @@
 package com.fges.ckonsoru;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
+import java.util.Properties;
 
 public class PostgresInteraction implements DatabaseInteraction {
 
@@ -25,4 +32,19 @@ public class PostgresInteraction implements DatabaseInteraction {
     public Integer delete(String table, String[] columns, String[] values) {
         return 1;
     }
+    String url = "jdbc:postgresql://localhost:5432/konsoru/";
+    Connection conn = DriverManager.getConnection(url);
+    
+    int foovalue = 1;
+    PreparedStatement st = conn.prepareStatement("SELECT * FROM disponibilite WHERE columnfoo = ?");
+    st.setInt(1, foovalue);
+    ResultSet rs = st.executeQuery();
+    while (rs.next())
+    {
+        System.out.print("Column 1 returned ");
+        System.out.println(rs.getString(1));
+    }
+    rs.close();
+    st.close();
+    
 }
