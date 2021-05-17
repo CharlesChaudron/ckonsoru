8f44318df57cad706f39c2521664426387416f7f
solo-carry <maelbizot23@gmail.com>
2021-05-17 07:41:48 +0200

    affichage listeAttente

--- src/main/java/com/fges/ckonsoru/dao/ListeAttenteDAO.java
+++ src/main/java/com/fges/ckonsoru/dao/ListeAttenteDAO.java
@@ -1,8 +1,12 @@
 package com.fges.ckonsoru.dao;
 
 import com.fges.ckonsoru.model.ListeAttente;
+import com.fges.ckonsoru.model.RendezVous;
+import java.util.List;
 
 public interface ListeAttenteDAO {
     
     public void creerListeAttente(ListeAttente listeAttente);
+
+    public List<RendezVous> listeAttentePourClient(String client);
 }
--- src/main/java/com/fges/ckonsoru/dao/postgres/ListeAttenteDaoPostgres.java
+++ src/main/java/com/fges/ckonsoru/dao/postgres/ListeAttenteDaoPostgres.java
@@ -6,6 +6,10 @@ import java.time.LocalDateTime;
 
 import com.fges.ckonsoru.dao.ListeAttenteDAO;
 import com.fges.ckonsoru.model.ListeAttente;
+import com.fges.ckonsoru.model.RendezVous;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListeAttenteDaoPostgres implements ListeAttenteDAO {
     protected PostgresConnexion postgresConnexion;
@@ -31,6 +35,39 @@ public class ListeAttenteDaoPostgres implements ListeAttenteDAO {
             System.err.println("Problème lors de la requête creerLsiteAttente");
             System.err.println(e.getMessage());
         }
+    }
+    
+    @Override
+    public List<RendezVous> listeAttentePourClient(String nomClient) {
+    
+        List<RendezVous> rdvs = new ArrayList<>();
+        
+        try {
+            PreparedStatement st = this.postgresConnexion.conn.prepareStatement(
+                    "SELECT la_client, la_numtel, la_dateauplustard, la_creneaupropose, la_datedemande \n" +
+                    "FROM listeattente\n" +
+                    "WHERE rv_client = ?\n" +
+                    "ORDER BY la_creneaupropose DESC;");
+            st.setObject(1, nomClient);
+            ResultSet rs = st.executeQuery();
+            while (rs.next())
+            {
+                rdvs.add(parseRdv(rs));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            System.err.println("Problème lors de la requête listeRendezVousPourClient");
+            System.err.println(ex.getMessage());
+        }
+        return rdvs;
+    }
+    private RendezVous parseRdv(ResultSet rs) throws SQLException {
+        RendezVous rdv = new RendezVous(
+                rs.getString("la_client"),
+                (LocalDateTime) rs.getObject("la_creneaupropose",LocalDateTime.class),
+                rs.getString("la_numtel"));
+        return rdv;
     }
 
     
